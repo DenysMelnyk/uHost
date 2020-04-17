0281d0b648cf838038c35e250edd526f6b0ea0ef
Denys Melnyk <denyscreativecraftagency@gmail.com>
2020-04-17 19:40:06 +0300

    add mobile nav and hide it - basic solution

--- shared.js
+++ shared.js
@@ -2,29 +2,32 @@ const selectPlanButtons = document.querySelectorAll(".plan .button");
 const modal = document.querySelector(".modal");
 const backdrop = document.querySelector(".backdrop");
 const cancelModal = document.querySelector(".modal__action--negative");
+const mobileButtonNavBtn = document.querySelector(".toggle-button");
+const mobileNav = document.querySelector(".mobile-nav");
 
-const toggleVisible = element => element.classList.toggle("visible");
+const toggleVisible = element => {
+  backdrop.classList.toggle("visible");
+  element.classList.toggle("visible");
+};
 
 const modalHandler = () => {
-  toggleVisible(backdrop);
   toggleVisible(modal);
+}
+
+const mobileNavHandler = () => {
+  toggleVisible(mobileNav);
+}
+
+const backdropHandler = () => {
+  backdrop.classList.remove("visible");
+  modal.classList.remove("visible");
+  mobileNav.classList.remove("visible");
 }
 
 for (let i = 0; i < selectPlanButtons.length; i++){
   selectPlanButtons[i].addEventListener("click", modalHandler);
 }
 
-// for(let selectPlanButton of selectPlanButtons){
-//   selectPlanButton.addEventListener("click", modalHandler);
-// }
-
-// selectPlanButtons.forEach(selectPlanButton => selectPlanButton.addEventListener("click", modalHandler));
-
-// let i = 0;
-// while (i < selectPlanButtons.length){
-//   selectPlanButtons[i].addEventListener("click", modalHandler);
-//   i++;
-// }
-
-backdrop.addEventListener("click", modalHandler);
+backdrop.addEventListener("click", backdropHandler);
 cancelModal.addEventListener("click", modalHandler);
+mobileButtonNavBtn.addEventListener("click", mobileNavHandler);
