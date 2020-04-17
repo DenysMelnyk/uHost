2120afd2f8e5b94faa8250d11f11a80e384ec3b0
Denys Melnyk <denyscreativecraftagency@gmail.com>
2020-04-17 18:26:54 +0300

    commented solutions (for of, while loops and forEach method)

--- shared.js
+++ shared.js
@@ -1,4 +1,4 @@
-const modalBtnOpenList = document.querySelectorAll(".plan .button");
+const selectPlanButtons = document.querySelectorAll(".plan .button");
 const modal = document.querySelector(".modal");
 const backdrop = document.querySelector(".backdrop");
 const cancelModal = document.querySelector(".modal__action--negative");
@@ -10,9 +10,21 @@ const modalHandler = () => {
   toggleVisible(modal);
 }
 
-for(let modalBtnOpen of modalBtnOpenList){
-  modalBtnOpen.addEventListener("click", modalHandler);
+for (let i = 0; i < selectPlanButtons.length; i++){
+  selectPlanButtons[i].addEventListener("click", modalHandler);
 }
+
+// for(let selectPlanButton of selectPlanButtons){
+//   selectPlanButton.addEventListener("click", modalHandler);
+// }
+
+// selectPlanButtons.forEach(selectPlanButton => selectPlanButton.addEventListener("click", modalHandler));
+
+// let i = 0;
+// while (i < selectPlanButtons.length){
+//   selectPlanButtons[i].addEventListener("click", modalHandler);
+//   i++;
+// }
 
 backdrop.addEventListener("click", modalHandler);
 cancelModal.addEventListener("click", modalHandler);
